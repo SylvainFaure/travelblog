c290c52551b46b6451f775cefbe2f75e927e4a79
SylvainFaure <sylvain.faure@live.fr>
2019-01-02 16:19:13 +0100

    Tests for auth

--- server/models/user.js
+++ server/models/user.js
@@ -10,6 +10,28 @@ class User {
 			cb(users)
 		})
 	}
+	
+	static getUsers(email, cb) {
+		db.query('SELECT * FROM users where user_email = ?', [email], function(err, rows){
+			if (err) throw err;
+			var records = JSON.stringify(rows[0]);
+			var user = JSON.parse(records);
+			cb(user)
+		})
+	}
+	
+	
+	static postUser(email, cb) {
+           const user = {
+	    email: email,
+            pwd: ''
+	   }
+	   db.query('INSERT INTO users SET ?', user, function(err, results, fields){
+			if (err) throw err;
+			cb(results)			
+	   })	
+	}
+	
 	static saveUserPwd(user, pwd, cb) {
 	   const user = {
 	    email: user.email,

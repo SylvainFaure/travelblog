1c12fde6d42f2bd571078fa8234b60df26c89168
SylvainFaure <sylvain.faure@live.fr>
2019-01-11 15:31:35 +0100

    Error handler init

---
+++ server/error-handler.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const config = require("./config.js")
+
+if (config.get('env') === 'development') {
+
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+  });
+
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});

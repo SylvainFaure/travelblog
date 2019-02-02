650b7ae4668b36c533795938afe04c51f4d0187f
SylvainFaure <sylvain.faure@live.fr>
2019-02-02 11:34:13 +0100

    Angular one app test 5

--- server/app.js
+++ server/app.js
@@ -261,16 +261,10 @@ app.delete('/api/article/unpublish/:id', (req, res) => {
 
 
 /*** ANGULAR ONE PAGE APP ***/
-if (app.get("env") == "development") {
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, viewPath, 'index.html'))
-	})
-}
-if (app.get("env") != "development") {
-	app.get('*', function(req, res) {
-    res.sendfile('index.html', {root: "../dist" })
-});
-}
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, viewPath, 'index.html'))
+})
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`)

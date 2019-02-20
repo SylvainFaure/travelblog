fb47b3623d264de83fca65572bdbdfa4fc049652
SylvainFaure <sylvain.faure@live.fr>
2019-02-20 11:12:48 +0100

    Refactoring route assets

---
+++ server/routes/assets.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const assets = express.Router()
+const Asset = require('../models/asset');
+const upload = require('./config/storage');
+
+assets.route('/')
+  .get((req, res) => {
+    Asset.getAll(allassets => {
+      res.json(allassets)
+    })
+  })
+  .post(upload.any('file'), (req, res) => {
+    Asset.uploadAssets(req.files, req.body.infos, result => {
+      res.json(result);
+    })
+  })
+  // TODO - Add a delete route
+
+assets.route('/:id')
+  .get((req, res) => {
+    // ROUTE TO WRITE (if needed)
+  })
+  .put((req, res) => {
+    Asset.updateAsset(req.body, req.params.id, asset => {
+      res.json(asset)
+    })
+  })
+  .delete((req, res) => {
+    Travel.deleteTravel(false, req.params.id, result => {
+      res.send(result);
+    })
+  })
+
+// TODO : change post in delete (was there any problem??)
+assets.route('/delete')
+  .delete((req, res) => {
+    Asset.deleteAssets(req.body.ids, req.body.names, results => {
+      res.json(results)
+    })
+  })
+
+
+module.exports = assets;

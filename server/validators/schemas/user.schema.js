6638916906a843d286cf21af453426e19178c3c6
SylvainFaure <sylvain.faure@live.fr>
2019-02-22 14:42:25 +0100

    Update user.schema.js

--- server/validators/schemas/user.schema.js
+++ server/validators/schemas/user.schema.js
@@ -1,22 +1,20 @@
 const Joi = require('joi');
-const userSchemas = {
-  signup: {
-    body: {
-        email: Joi.string().email().required(),
-        password: Joi.string().required()
-    }
-  },
-  signin: {
-    body: {
-        email: Joi.string().email().required(),
-        password: Joi.string().required()
-    }
-  },
-  request: {
-    body: {
-      email: Joi.string().email().required(),
-      role: Joi.string().required().valid('admin', 'visitor', 'editor')
-    }
-  }
-}
+
+const signup = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
+
+const signin = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+});
+
+const request = Joi.object({
+  email: Joi.string().email().required(),
+  role: Joi.string().required().valid('admin', 'visitor', 'editor')
+})
+
+const userSchemas = { signup, signin, request };
+
 module.exports = userSchemas; 

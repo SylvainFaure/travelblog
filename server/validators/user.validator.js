f1b15b6eb86e8e9d92b914ed4fea335f3ca558b4
SylvainFaure <sylvain.faure@live.fr>
2019-02-22 15:07:39 +0100

    User validator draft

--- server/validators/user.validator.js
+++ server/validators/user.validator.js
@@ -1,8 +1,19 @@
-import expressJoi from 'express-joi-validator';
+import Joi from 'joi';
 import userSchema from './schemas/user.schema';
 
-validate = (method) => {    
-     return expressJoi(userSchema[method])
+validate = (value, method) => {  
+ const schema = {
+  body: userSchema[method]
+ }
+ return new Promise((res, rej) => {
+   Joi.validate(value, schema, (err, value) => {
+     if (!err) {
+      resolve(value);
+     } else {
+      reject(err);
+     }
+   })  
+ })
 }
 
 module.exports = validate;

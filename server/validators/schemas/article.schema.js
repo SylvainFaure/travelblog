3eaa7f34099dcd2aa47ca349f3d310182eafee22
Sylvain Faure <sylvain.faure@live.fr>
2019-03-05 14:55:13 +0100

    Add publish schema

--- server/validators/schemas/article.schema.js
+++ server/validators/schemas/article.schema.js
@@ -1,8 +1,5 @@
 const Joi = require('joi');
 
-const createObj = {
-  
-}
 const create = Joi.alternatives().try(
   Joi.object().keys({
     article_title_fr: Joi.string().allow(''),
@@ -56,6 +53,60 @@ const create = Joi.alternatives().try(
   })
 );
 
-const articleSchemas = { create };
+const publish = Joi.alternatives().try(
+  Joi.object().keys({
+    article_id: Joi.number().required(),
+    article_title_fr: Joi.string().allow(''),
+    article_title_it: Joi.string().required(),
+    article_travel_id: Joi.number().required(),
+    article_country_fr: Joi.string().allow(''),
+    article_country_it: Joi.string().required(),
+    article_place_fr: Joi.string().allow(''),
+    article_place_it: Joi.string().required(),
+    article_slug: Joi.string().required(), // todo: generate it if not present
+    article_catch_phrase_fr: Joi.string().allow(''),
+    article_catch_phrase_it: Joi.string().allow(''),
+    article_short_desc_fr: Joi.string().allow(''),
+    article_short_desc_it: Joi.string().required(),
+    article_long_desc_fr: Joi.string().allow(''),
+    article_long_desc_it: Joi.string().required(),
+    article_gallery_fr: Joi.string().allow(''),
+    article_gallery_it: Joi.string().allow(''),
+    article_cover: Joi.string().required(),
+    article_date_from: Joi.number().required(),
+    article_date_to: Joi.number().required(),
+    article_published_date_fr: Joi.string().allow(''),
+    article_published_date_it: Joi.string().required(),
+    article_published_fr: Joi.bool(),
+    article_published_it: Joi.bool().required()
+  }),
+  Joi.object().keys({
+    article_title_fr: Joi.string().required(),
+    article_title_it: Joi.string().allow(''),
+    article_travel_id: Joi.number().required(),
+    article_country_fr: Joi.string().required(),
+    article_country_it: Joi.string().allow(''),
+    article_place_fr: Joi.string().required(),
+    article_place_it: Joi.string().allow(''),
+    article_slug: Joi.string().required(), // todo: generate it if not present
+    article_catch_phrase_fr: Joi.string().allow(''),
+    article_catch_phrase_it: Joi.string().allow(''),
+    article_short_desc_fr: Joi.string().required(),
+    article_short_desc_it: Joi.string().allow(''),
+    article_long_desc_fr: Joi.string().required(),
+    article_long_desc_it: Joi.string().allow(''),
+    article_gallery_fr: Joi.string().allow(''),
+    article_gallery_it: Joi.string().allow(''),
+    article_cover: Joi.string().required(),
+    article_date_from: Joi.number().required(),
+    article_date_to: Joi.number().required(),
+    article_published_date_fr: Joi.string().required(),
+    article_published_date_it: Joi.string().allow(''),
+    article_published_fr: Joi.bool().required(),
+    article_published_it: Joi.bool()
+  })
+);
+
+const articleSchemas = { create, publish };
 
 module.exports = articleSchemas; 

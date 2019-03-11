703c0b5e1e181890b7a0f1e47da37ae9d404d381
Sylvain Faure <sylvain.faure@live.fr>
2019-03-11 12:03:20 +0100

    Role validator accepts array

--- server/validators/role.validator.js
+++ server/validators/role.validator.js
@@ -1,13 +1,16 @@
 validateRole = (token, roleToValidate) => {  
-  
- return new Promise((res, rej) => {
-   const decodeToken = Buffer.from(token.split('.')[1], 'base64').toString('binary');
-   const role = JSON.parse(decodeToken).role
-   if (role !== roleToValidate) {
-     reject()
-   } else {
-     resolve()
-   }
- });
+  if (!Array.isArray(roleToValidate)) {
+    roleToValidate = [roleToValidate]
+  }
+  return new Promise((res, rej) => {
+    const decodeToken = Buffer.from(token.split('.')[1], 'base64').toString('binary');
+    const role = JSON.parse(decodeToken).role
+    if (roleToValidate.indexOf(role) == -1) {
+      reject()
+    } else {
+      resolve()
+    }
+  });
+}
 
 module.exports = validateRole;

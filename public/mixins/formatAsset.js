90e83aa870e092c2b0448ceb80e284b9a74f53cc
Sylvain Faure <sylvain.faure@develondigital.com>
2019-05-14 18:22:56 +0200

    formatAsse mixin and transition css

---
+++ public/mixins/formatAsset.js
@@ -0,0 +1,18 @@
+export default {
+  methods: {
+    formatAsset(asset) {
+      const lang = 'fr'
+      if (lang === 'fr') {
+        asset.src = asset.asset_src
+        asset.title = asset.asset_title_fr
+        asset.place = asset.asset_place_fr
+      }
+      if (lang === 'it') {
+        asset.src = asset.asset_src
+        asset.title = asset.asset_title_it
+        asset.place = asset.asset_place_it
+      }
+      return asset
+    }
+  }
+}

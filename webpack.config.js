72bc234553d8f44cdd3dbefe569c3195b0a020a9
Sylvain <sylvain.faure@live.fr>
2019-09-05 18:13:19 +0200

    debug

--- webpack.config.js
+++ webpack.config.js
@@ -99,7 +99,6 @@ const plugins = [
     {from: './admin/js/vendors/semantic/semantic.min.css', to: 'admin/vendors/semantic'},
 
     {from: './server', to: 'server'},
-    {from: './.nuxt', to: '.nuxt'},
     {from: './public', to: 'public'}
   ])
 ]

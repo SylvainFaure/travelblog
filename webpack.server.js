21c4039ee6f86262b3c920969e5bbcf9b13fb279
Sylvain Faure <sylvain.faure@live.fr>
2022-12-09 15:16:34 +0100

    modify output

--- webpack.server.js
+++ webpack.server.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'functions'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: 'app.js'
   },
   target: 'node',
   node: {

f02813a53ab5195518098019b267731c59b06990
Sylvain Faure <sylvain.faure@live.fr>
2022-12-09 15:22:24 +0100

    rename env variables in conflict with netlify

--- dist/server/config/aws.js
+++ dist/server/config/aws.js
@@ -1,8 +1,8 @@
 const aws = require('aws-sdk');
 
 aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SEC_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACC_KEY_ID,
   // region: 'eu-west-3'
 });
 

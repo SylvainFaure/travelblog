0651713dea7da153e13b69fd573f058a35c0d63d
Sylvain Faure <sylvain.faure@live.fr>
2022-12-16 10:05:19 +0100

    modify api config

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ export default async function ({ $axios, store, redirect, route }) {
   const user = JSON.parse(window.localStorage.getItem('user'))
   const token = user && user.token ? user.token : store.state.auth.token
   const res = await $axios.post('/api/users/verifytoken', { token })
+  console.log('VERIFY TOKEN', res.data)
   const isValid = !['JsonWebTokenError', 'TokenExpiredError'].includes(res.data.name)
   if (!isValid) {
     store.commit('auth/setIsLogged', false)
